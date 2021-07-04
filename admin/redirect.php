579eee534952ec9ba68b8cf1fdfee4a9a132c379
PayalBangad <bangadp2@gmail.com>
2021-07-04 23:41:36 +0530

    My first Project
    Hotel Management System

---
+++ admin/redirect.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<br><br><br>
+<p>Room Unbook Successfully....<a href='http://localhost/saffron/index.html'>Click here to go to home page</a></p>
+</body>
+</html>
